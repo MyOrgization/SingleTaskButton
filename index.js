a464c9dea89d6b2603768b0f7906e8c767653ded
Uncle Charlie <lixiangdong@haoqix.com>
2017-02-14 10:18:04 +0800

    single task algirithm

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+/** 
+ * Created by Uncle Charlie, 2017/02/
+*/
+
 import React from 'react'
 import {
   View,
@@ -5,16 +9,27 @@ import {
   TouchableOpacity,
 } from 'react-native'
 
+import singleTaskKeeper from './lib/singleTaskKeeper'
+
+const THRESHOLD_WAIT = 300
+
 export default class SingleTaskButton extends React.Component {
   constructor(props) {
     super(props)
+
+    this._startTask = singleTaskKeeper(this._startTask, THRESHOLD_WAIT, this)
+  }
+
+  _startTask() {
+    this.props.onPress()
   }
 
   render() {
     return (
-      <TouchableOpacity>
-        <Text>Example</Text>
+      <TouchableOpacity activeOpacity={1} style={this.props.style} onPress={this._startTask}>
+        {this.props.children}
       </TouchableOpacity>
     )
   }
 }
+
